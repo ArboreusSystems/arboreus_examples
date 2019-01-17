bcee8e52fc3fce6666ef8bb57337f6a86bf4d22f
Alexandr Kirilov <alexandr.kirilov@post.com>
2019-01-17 14:29:26 +0300

    ARBXMPL-8 Publish C++ example: Inheritance

---
+++ cpp/inheritance_v01.cpp
@@ -0,0 +1,55 @@
+/* ------------------------------------------
+ * @author Alexandr KIRILOV
+ * @copyright (C) 2019, http://arboreus.system
+ * @doc Arboreus C++ Examples. Class inheritance.
+ *
+ * @end
+ * Created : 01/17/2019 at 09:01
+*///-----------------------------------------
+
+#include <iostream>
+
+using namespace std;
+
+
+// Parent Class
+class Parent{
+	public:
+		void Set_x(int x){
+			Parent::x = x;
+		}
+		int Get_x(){
+			return x;
+		}
+	private:
+		int x;
+};
+
+
+// Child Class
+class Child : public Parent {
+	public:
+		void Set_y(int y){
+			Child::y = y;
+		}
+		int Get_y(){
+			return y;
+		}
+	private:
+		int y;
+};
+
+
+// Test application
+int main(){
+
+	Child Child;
+	Child.Set_x(1);
+	Child.Set_y(2);
+
+	cout << "Child.X: " << Child.Get_x() << endl;
+	cout << "Child.Y: " << Child.Get_y() << endl;
+
+	cout << "Hello Inheritance!" << endl;
+	return 0;
+}
