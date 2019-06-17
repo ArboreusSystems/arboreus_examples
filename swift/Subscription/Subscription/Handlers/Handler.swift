2630fe52cf7323725c95e5f73395fe08458b6a8f
Alexandr Kirilov <alexandr.kirilov@post.com>
2019-06-17 15:01:06 +0300

    ARBXMPL-42 Intermediate commit

---
+++ swift/Subscription/Subscription/Handlers/Handler.swift
@@ -0,0 +1,13 @@
+//
+//  Handler.swift
+//  Subscription
+//
+//  Created by Alexandr Kirilov on 17/06/2019.
+//  Copyright © 2019 none. All rights reserved.
+//
+
+import UIKit
+
+class Handler: NSObject {
+
+}
---
+++ swift/Subscription/Subscription/Subscription/Subscription.swift
@@ -0,0 +1,25 @@
+//
+//  Subscription.swift
+//  Subscription
+//
+//  Created by Alexandr Kirilov on 17/06/2019.
+//  Copyright © 2019 none. All rights reserved.
+//
+
+import UIKit
+
+class Subscription: NSObject {
+
+	static func mSubscriptionStatus() -> Bool {
+		
+		return false;
+	}
+	
+	static func mSubscribe() -> Bool {
+	
+		print("Subscription:mSubscribe()");
+		return true;
+	}
+	
+	
+}
