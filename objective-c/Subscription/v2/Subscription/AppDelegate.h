aa764d09bdfed4e8e2deb1d161dd8bc80c65a1b9
Alexandr Kirilov <alexandr.kirilov@post.com>
2019-06-27 22:01:03 +0300

    ARBXMPL-45 Intermediate commit

---
+++ objective-c/Subscription/v2/Subscription/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Subscription
+//
+//  Created by Alexandr Kirilov on 27/06/2019.
+//  Copyright © 2019 none. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
