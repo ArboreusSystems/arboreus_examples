d745e777f0091744ff73165a54dfe1b0f32b7ec0
Alexandr Kirilov <alexandr.kirilov@post.com>
2019-07-15 17:13:47 +0300

    ARBXMPL-56 Reconfigure repository

---
+++ swift/Custom/Custom Navigation Bar/Custom Navigation Bar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Custom Navigation Bar
+//
+//  Created by Alexandr Kirilov on 15/07/2019.
+//  Copyright © 2019 Arboreus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
