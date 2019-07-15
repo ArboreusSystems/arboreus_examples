c48d647087e881ef68e39eaf85d0511c5807d2b4
Alexandr Kirilov <alexandr.kirilov@post.com>
2019-07-15 18:18:13 +0300

    ARBXMPL-55 Initiate

---
+++ swift/Custom/Custom Navigation Bar/Custom Navigation Bar/ViewControllers/VCMain.swift
@@ -0,0 +1,18 @@
+//
+//  VCMain.swift
+//  Custom Navigation Bar
+//
+//  Created by Alexandr Kirilov on 15/07/2019.
+//  Copyright © 2019 Arboreus. All rights reserved.
+//
+
+import UIKit
+
+class VCMain: UIViewController {
+
+	override func loadView() {
+		
+		super.loadView();
+		self.view.backgroundColor = UIColor.white;
+	}
+}
