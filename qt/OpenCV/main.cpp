abdede2b580e86bc41ec59658d5ffe8b11cb9848
Alexandr Kirilov <alexandr.kirilov@post.com>
2019-09-19 11:07:22 +0300

    ARBXMPL-85 Initiate

---
+++ qt/OpenCV/main.cpp
@@ -0,0 +1,33 @@
+/* -------------------------------------------------------------------
+ *  @doc
+ *  @notice Template file tablemodel.h
+ *
+ *  @copyright Arboreus (http://arboreus.systems)
+ *  @author Alexandr Kirilov (http://alexandr.kirilov.me)
+ *  @created 19/09/2019 at 11:01:26
+ * */// --------------------------------------------------------------
+
+// System includes
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+
+
+// Application includes
+
+// Constants
+const QString Main = "qrc:/main.qml";
+
+
+// QtQuick Application
+int main(int Counter, char *Arguments[]) {
+
+	QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+	QGuiApplication Application(Counter, Arguments);
+
+	QQmlApplicationEngine Engine;
+	Engine.load(QUrl(Main));
+	if (Engine.rootObjects().isEmpty())
+		return -1;
+
+	return Application.exec();
+}
