8fbeb192917fb6546b5d60bb9e9513b353a959eb
Alexandr Kirilov <alexandr.kirilov@post.com>
2019-10-26 22:06:56 +0300

    ARBXMPL-95 Publish Qt example: QHashStorage v3

---
+++ qt/QHashObjectStore/v3/dispatcher.h
@@ -0,0 +1,47 @@
+#ifndef DISPATCHER_H
+#define DISPATCHER_H
+
+#include <QObject>
+#include <QHash>
+#include <QString>
+#include <objectprimitive.h>
+#include <object1.h>
+#include <object2.h>
+#include <object3.h>
+#include <QVariantList>
+#include <QVariant>
+#include <QList>
+
+#include "aglobal.h"
+
+class Dispatcher : public QObject {
+
+	Q_OBJECT
+
+	public:
+
+		static Dispatcher& mInstance(void) {
+			static Dispatcher oSingletone;
+			return oSingletone;
+		}
+
+		enum class ObjectTypes {
+			Object1 = 1,
+			Object2 = 2,
+			Object3 = 3
+		}; Q_ENUM(ObjectTypes)
+
+		bool mCreate(QString inName,ObjectTypes inType);
+		QHash<bool,ObjectPrimitive*> mGetObject(QString inName);
+		ObjectPrimitive* mGetSimpleObject(QString inName);
+		QVariantList mGetListOfObjects(void);
+
+	private:
+
+		explicit Dispatcher(QObject *parent = nullptr);
+		virtual ~Dispatcher(void);
+
+		QHash<QString,ObjectPrimitive*> pObjects;
+};
+
+#endif // DISPATCHER_H
---
+++ qt/QHashObjectStore/v3/object1.h
@@ -0,0 +1,14 @@
+#ifndef OBJECT1_H
+#define OBJECT1_H
+
+#include <QObject>
+#include "objectprimitive.h"
+
+class Object1 : public ObjectPrimitive {
+
+	public:
+		explicit Object1(QObject *parent = nullptr);
+		virtual ~Object1(void);
+};
+
+#endif // OBJECT1_H
