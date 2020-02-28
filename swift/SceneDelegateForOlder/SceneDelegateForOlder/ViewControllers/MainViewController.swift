1250659d78dcd46af976ff1eabbc4d711ce0fae0
Alexandr Kirilov <alexandr.kirilov@post.com>
2020-02-28 21:33:32 +0300

    ARBXMPL-111 Publish Swift example: Application Scene Delegate for older iOS versions

---
+++ swift/SceneDelegateForOlder/SceneDelegateForOlder/ViewControllers/MainViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MainViewController.swift
+//  SceneDelegateForOlder
+//
+//  Created by Alexandr Kirilov on 28/02/2020.
+//  Copyright © 2020 Alexandr Kirilov. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        
+		super.viewDidLoad();
+		view.backgroundColor = _COLOR_BLUE;
+    }
+}
