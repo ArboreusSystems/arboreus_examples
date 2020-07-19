9b857e715767249b7d27ef9c764c26dfe0aadf43
Alexandr Kirilov <alexandr.kirilov@post.com>
2020-07-19 17:34:18 +0300

    ARBXMPL-122 Publish Objective-C example: MobileVLCKit implementation

---
+++ cpp/Pointers/Pointers_v1/main.cpp
@@ -0,0 +1,26 @@
+/* -------------------------------------------------------------------
+ *  @doc
+ *  @notice Template file wizards/projects/plaincpp/main.cpp
+ *
+ *  @copyright Arboreus (http://arboreus.systems)
+ *  @author Alexandr Kirilov (http://alexandr.kirilov.me)
+ *  @created 19/07/2020 at 15:16:17
+ * */// --------------------------------------------------------------
+
+// System includes
+#include <iostream>
+
+// Namespace
+using namespace std;
+
+// Application includes
+
+// Constants
+
+
+// Application
+int main(int inCounter, char *inArguments[]) {
+
+	cout << "Hello World!" << endl;
+	return 0;
+}
