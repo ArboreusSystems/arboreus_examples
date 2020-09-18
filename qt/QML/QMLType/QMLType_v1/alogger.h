122a42ff9e7b7f32028e3d1ed3ee8635e4ec1118
Alexandr Kirilov <alexandr.kirilov@post.com>
2020-09-18 13:32:22 +0300

    ARBXMPL-168 Publish Qt example: Custom QML Type v1

---
+++ qt/QML/QMLType/QMLType_v1/alogger.h
@@ -0,0 +1,49 @@
+// ----------------------------------------------------------
+/*!
+	\headerfile ALogger
+	\title
+	\brief Template file wizard/classes/cpp/file.h
+
+	\list
+	\li @notice Template file classes/file.h
+	\li @copyright Arboreus (http://arboreus.systems)
+	\li @author Alexandr Kirilov (http://alexandr.kirilov.me)
+	\li @created 16/09/2020 at 17:33:53
+	\endlist
+*/
+// ----------------------------------------------------------
+
+#ifndef ALOGGER_H
+#define ALOGGER_H
+
+// System includes
+#include <QObject>
+#include <QDateTime>
+#include <QDebug>
+
+// Application includes
+
+// Constants and definitions
+#define ALOG qDebug() << QDateTime::currentMSecsSinceEpoch()
+#define ALOG_SYSTEM ALOG << "[SYSTEM]"
+#define ALOG_QML ALOG << "[QML]"
+
+// Namespace
+
+
+// Class definitions
+class ALogger : public QObject {
+
+	Q_OBJECT
+
+	public:
+
+		explicit ALogger(QObject *parent = nullptr);
+		virtual ~ALogger(void);
+
+	public slots:
+
+		void mWriteToLog(QString inMessage);
+};
+
+#endif // ALOGGER_H
