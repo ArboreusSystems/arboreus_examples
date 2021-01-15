d58d122763310498b9f36aeab0f69c65196c2872
Alexandr Kirilov <alexandr.kirilov@post.com>
2021-01-15 19:40:57 +0300

    ARBXMPL-193 Publish Qt example: Using Qt Creator for BASH Scripting project

---
+++ qt/Bash/Bash_v1/script1.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+source script_include.sh;
+
+TEST_VARIABLE="Test_variable_value";
+
+echo "VAR1: ${VAR1}";
+echo "VAR2: ${VAR2}";
+echo "VAR3: ${VAR3}";
+echo "TEST_VARIABLE: ${TEST_VARIABLE}";
+
+pwd;
+ls -la;
