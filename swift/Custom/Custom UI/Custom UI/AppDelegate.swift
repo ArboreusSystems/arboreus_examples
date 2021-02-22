4bc5714c2ab51ab976aefd2f2a3cbc4a63b8f7b2
Alexandr Kirilov <alexandr.kirilov@post.com>
2021-02-22 17:58:15 +0300

    ARBXMPL-202 Fix statusBar color settings in Swift Custom UI example

--- swift/Custom/Custom UI/Custom UI/AppDelegate.swift
+++ swift/Custom/Custom UI/Custom UI/AppDelegate.swift
@@ -13,13 +13,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 	var window: UIWindow?
 
-
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-
-		let oStatusBar: UIView = UIApplication.shared.value(forKey: "statusBar") as! UIView;
-		if (oStatusBar.responds(to: #selector(setter: UIView.backgroundColor))) {
-			oStatusBar.backgroundColor = _COLOR_PURPLE;
-		}
 
 		window = UIWindow(frame: UIScreen.main.bounds);
 		self.window?.rootViewController = MainViewController() as UIViewController;
@@ -38,4 +32,38 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 		
 		return true;
 	}
+}
+
+extension UIApplication {
+
+var statusBarUIView: UIView? {
+	
+    if #available(iOS 13.0, *) {
+        let oTag = 150877;
+        let oKeyWindow = UIApplication.shared.connectedScenes
+        	.filter({$0.activationState == .foregroundActive})
+        	.map({$0 as? UIWindowScene})
+        	.compactMap({$0})
+        	.first?.windows
+        	.filter({$0.isKeyWindow}).first;
+
+        if let oStatusBar = oKeyWindow?.viewWithTag(oTag) {
+        	oStatusBar.backgroundColor = _COLOR_PURPLE;
+            return oStatusBar;
+        } else {
+            guard let oStatusBarFrame = oKeyWindow?.windowScene?.statusBarManager?.statusBarFrame else { return nil }
+            let oStatusBar = UIView(frame: oStatusBarFrame);
+            oStatusBar.backgroundColor = _COLOR_PURPLE;
+            oStatusBar.tag = oTag;
+            oKeyWindow?.addSubview(oStatusBar);
+            return oStatusBar;
+        }
+    } else if responds(to: Selector(("statusBar"))) {
+		let oStatusBar: UIView = (value(forKey: "statusBar") as? UIView)!;
+		oStatusBar.backgroundColor = _COLOR_PURPLE;
+        return oStatusBar;
+    } else {
+        return nil
+    }
+  }
 }
