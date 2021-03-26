691b8aadb15f3746dd536dc31cf873243a39e8e6
Alexandr Kirilov <alexandr.kirilov@post.com>
2021-03-26 16:05:35 +0300

    ARBXMPL-216 Publish Qt example: Sqlite v4.

---
+++ qt/Sqlite/Sqlite_v4/aobjectservice.h
@@ -0,0 +1,51 @@
+// ----------------------------------------------------------
+/*!
+	\headerfile AObjectService
+	\title
+	\brief Template file wizard/classes/cpp/file.h
+
+	\list
+	\li @notice Template file classes/file.h
+	\li @copyright Arboreus (http://arboreus.systems)
+	\li @author Alexandr Kirilov (http://alexandr.kirilov.me)
+	\li @created 26/03/2021 at 14:55:23
+	\endlist
+*/
+// ----------------------------------------------------------
+
+#ifndef AOBJECTSERVICE_H
+#define AOBJECTSERVICE_H
+
+// System includes
+#include <QObject>
+
+// Application includes
+#include <alogger.h>
+#include <athreadservicetemplate.h>
+#include <adbsqlite.h>
+#include <aproperties.h>
+
+// Constants and definitions
+
+// Namespace
+
+// Class definitions
+class AObjectService : public AThreadServiceTemplate {
+
+	Q_OBJECT
+
+	public:
+
+		explicit AObjectService(QObject *parent = nullptr);
+		virtual ~AObjectService(void);
+
+	public slots:
+
+		void slInitService(void);
+
+	private:
+
+		ADbSqlite* pDatabase = nullptr;
+};
+
+#endif // AOBJECTSERVICE_H
