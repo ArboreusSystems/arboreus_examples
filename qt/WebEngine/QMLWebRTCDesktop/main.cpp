b710b3157ea8a7a43a5631a85655db8a9a65c906
Alexandr Kirilov <alexandr.kirilov@post.com>
2021-05-22 10:05:52 +0300

    ARBXMPL-234 Publish Qt example: WebRTC for Desktop.

---
+++ qt/WebEngine/QMLWebRTCDesktop/main.cpp
@@ -0,0 +1,42 @@
+/* -------------------------------------------------------------------
+ *  @doc
+ *  @notice Template file tablemodel.h
+ *
+ *  @copyright Arboreus (http://arboreus.systems)
+ *  @author Alexandr Kirilov (http://alexandr.kirilov.me)
+ *  @created 17/09/2019 at 16:04:18
+ * */// --------------------------------------------------------------
+
+// System includes
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+
+// Application includes
+
+// Constants
+const QString Main = "qrc:/main.qml";
+
+
+// QtQuick Application
+int main(int inCounter, char *inArguments[]) {
+
+#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
+	QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+#endif
+
+	QGuiApplication oApplication(inCounter, inArguments);
+	QQmlApplicationEngine oEngine;
+
+	const QUrl oURL(QStringLiteral("qrc:/main.qml"));
+	QObject::connect(
+		&oEngine, &QQmlApplicationEngine::objectCreated,
+		&oApplication, [oURL](QObject *obj, const QUrl &objUrl) {
+			if (!obj && oURL == objUrl) {
+				QCoreApplication::exit(-1);
+			}
+		}, Qt::QueuedConnection
+	);
+	oEngine.load(oURL);
+
+	return oApplication.exec();
+}
