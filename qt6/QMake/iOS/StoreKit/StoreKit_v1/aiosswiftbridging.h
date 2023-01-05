b7f4b01ba76b0d2652bb7a100f6c30f52f2dfa0f
Alexandr Kirilov <alexandr.kirilov@post.com>
2023-01-05 22:47:00 +0300

    ARBXMPL-324 Intermediate commit

---
+++ qt6/QMake/iOS/StoreKit/StoreKit_v1/aiosswiftbridging.h
@@ -0,0 +1,26 @@
+// ----------------------------------------------------------
+/*!
+	\headerfile
+	\title
+	\brief Template file files/cppheader/file.h
+
+	\list
+	\li @notice Template file classes/file.h
+	\li @copyright Arboreus (http://arboreus.systems)
+	\li @author Alexandr Kirilov (http://alexandr.kirilov.me)
+	\li @created 05/01/2023 at 22:34:27
+	\endlist
+*/
+// ----------------------------------------------------------
+#ifndef AIOSSWIFTBRIDGING_H
+#define AIOSSWIFTBRIDGING_H
+
+// System includes
+
+// Application includes
+
+// Constants and defintions
+
+// Namespace
+
+#endif // AIOSSWIFTBRIDGING_H
---
+++ qt6/QMake/iOS/StoreKit/StoreKit_v1/amainpch.h
@@ -0,0 +1,34 @@
+// ----------------------------------------------------------
+/*!
+	\headerfile
+	\title
+	\brief Template file files/cppheader/file.h
+
+	\list
+	\li @notice Template file classes/file.h
+	\li @copyright Arboreus (http://arboreus.systems)
+	\li @author Alexandr Kirilov (http://alexandr.kirilov.me)
+	\li @created 05/01/2023 at 22:41:00
+	\endlist
+*/
+// ----------------------------------------------------------
+#ifndef AMAINPCH_H
+#define AMAINPCH_H
+
+#if defined __cplusplus
+
+#pragma once
+
+// System includes
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+
+// Application includes
+
+// Constants and defintions
+
+// Namespace
+
+#endif
+
+#endif // AMAINPCH_H
