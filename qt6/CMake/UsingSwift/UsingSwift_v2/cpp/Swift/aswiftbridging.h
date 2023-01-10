ccbd93bc0b353ef851f7d65542e0f970f43cbd70
Alexandr Kirilov <alexandr.kirilov@post.com>
2023-01-10 20:12:45 +0300

    ARBXMPL-327 Publish Qt example: Using Swift with Qt 6 and CMake v2

---
+++ qt6/CMake/UsingSwift/UsingSwift_v2/cpp/Swift/aswiftbridging.h
@@ -0,0 +1,26 @@
+// ----------------------------------------------------------
+/*!
+	\headerfile
+	\title
+	\brief Template file files/cppheader/file.h
+
+	\list
+	\li @notice Template file classes/file.h
+	\li @copyright Arboreus (http://arboreus.systems)
+	\li @author Alexandr Kirilov (http://alexandr.kirilov.me)
+	\li @created 17/12/2022 at 22:56:51
+	\endlist
+*/
+// ----------------------------------------------------------
+#ifndef ASWIFTBRIDGING_H
+#define ASWIFTBRIDGING_H
+
+// System includes
+
+// Application includes
+
+// Constants and defintions
+
+// Namespace
+
+#endif // ASWIFTBRIDGING_H
