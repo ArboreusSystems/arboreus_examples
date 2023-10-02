cb058a4b1dec57b7ded65dc77adb0c74e324f615
Alexandr Kirilov <alexandr.kirilov@post.com>
2023-10-02 20:05:26 +0300

    ARBXMPL-340 Intermediate commit

--- cpp/FFMPEG/FFMPEG_v2/main.cpp
+++ cpp/FFMPEG/FFMPEG_v2/main.cpp
@@ -19,7 +19,6 @@ extern "C" {
 #include <stdio.h>
 #include <libavcodec/avcodec.h>
 #include <libavformat/avformat.h>
-#include <libavutil/imgutils.h>
 
 #ifdef __cplusplus
 } // closing brace for extern "C"
@@ -34,7 +33,6 @@ extern "C" {
 static void gLogging(const char* inMessage, ...);
 static int gDecodePacket(AVPacket* inPacket, AVCodecContext* inCodecContext, AVFrame* inFrame);
 static void gSaveGrayFrame(unsigned char* inBuffer, int inWrap, int inXSize, int inYSize, char* inFilename);
-int WriteJPEG (AVCodecContext *pCodecCtx, AVFrame *pFrame, int FrameNo);
 
 // Constants
 #define A_UNUSED(inVariable) (void)inVariable;
@@ -57,72 +55,6 @@ static void gLogging(const char* inMessage, ...) {
 	va_end(oArguments);
 	fprintf(stderr,"\n");
 }
-
-
-int WriteJPEG (AVCodecContext *pCodecCtx, AVFrame *pFrame, int FrameNo) {
-
-	gLogging("Writing frame: %d",FrameNo);
-
-	AVCodecContext *pOCodecCtx;
-	AVCodec *pOCodec;
-	uint8_t *Buffer;
-	int BufSiz;
-	int BufSizActual;
-	int ImgFmt = AV_PIX_FMT_RGB24; //for the newer ffmpeg version, this int to pixelformat
-
-	FILE *JPEGFile;
-	char JPEGFName[256];
-
-	BufSiz = av_image_get_buffer_size(ImgFmt,pCodecCtx->width,pCodecCtx->height,1);
-	Buffer = (uint8_t *)malloc ( BufSiz );
-	if ( Buffer == NULL ) return ( 0 );
-	memset ( Buffer, 0, BufSiz );
-
-	pOCodecCtx = avcodec_alloc_context3(pCodecCtx );
-	if ( !pOCodecCtx ) {
-		free ( Buffer );
-		return ( 0 );
-	}
-
-	pOCodecCtx->bit_rate = pCodecCtx->bit_rate;
-	pOCodecCtx->width = pCodecCtx->width;
-	pOCodecCtx->height = pCodecCtx->height;
-	pOCodecCtx->pix_fmt = ImgFmt;
-	pOCodecCtx->codec_id = AV_CODEC_ID_MJPEG;
-	pOCodecCtx->codec_type = AVMEDIA_TYPE_VIDEO;
-	pOCodecCtx->time_base.num = pCodecCtx->time_base.num;
-	pOCodecCtx->time_base.den = pCodecCtx->time_base.den;
-
-	pOCodec = const_cast<AVCodec*>(avcodec_find_encoder ( pOCodecCtx->codec_id ));
-	if ( !pOCodec ) {
-		free ( Buffer );
-		return ( 0 );
-	}
-
-	if ( avcodec_open2 ( pOCodecCtx, pOCodec, NULL ) < 0 ) {
-		free ( Buffer );
-		return ( 0 );
-	}
-
-	pOCodecCtx->mb_lmin = pOCodecCtx->qmin * FF_QP2LAMBDA;
-	pOCodecCtx->mb_lmax = pOCodecCtx->qmax * FF_QP2LAMBDA;
-	pOCodecCtx->flags = AV_CODEC_FLAG_QSCALE;
-	pOCodecCtx->global_quality = pOCodecCtx->qmin * FF_QP2LAMBDA;
-
-	pFrame->pts = 1;
-	pFrame->quality = pOCodecCtx->global_quality;
-	BufSizActual = avcodec_encode_video2(pOCodecCtx,Buffer,BufSiz,pFrame);
-
-	sprintf ( JPEGFName, "%06d.jpg", FrameNo );
-	JPEGFile = fopen ( JPEGFName, "wb" );
-	fwrite ( Buffer, 1, BufSizActual, JPEGFile );
-	fclose ( JPEGFile );
-
-	avcodec_close ( pOCodecCtx );
-	free ( Buffer );
-	return ( BufSizActual );
-}
-
 
 
 // -----------
@@ -328,8 +260,6 @@ int main(int inCounter, const char* inArguments[]) {
 					oInputFrame->display_picture_number
 				);
 			}
-
-			WriteJPEG(oInputCodecVideoContext,oInputFrame,oInputCodecVideoContext->frame_number);
 		}
 
 		if (oInputPacket->stream_index == oInputAudioStreamIndex) {
