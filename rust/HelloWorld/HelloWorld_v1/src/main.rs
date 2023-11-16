fcd987447033cd2df36078f3e125c0e6caa11622
Alexandr Kirilov <alexandr.kirilov@post.com>
2023-11-16 21:54:52 +0300

    ARBXMPL-355 Publish Rust example: Hello World v1

---
+++ rust/HelloWorld/HelloWorld_v1/src/main.rs
@@ -0,0 +1,15 @@
+// --------------------------------------
+// Test application
+// --------------------------------------
+
+// System includes
+
+// Application
+#![allow(non_snake_case)]
+fn main() {
+
+	let oHello: String = String::from("Hello");
+	let oWorld: &str = "World";
+
+	println!("{oHello} {oWorld}!");
+}
