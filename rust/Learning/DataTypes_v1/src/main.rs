7eba0771c42c983e3fd5ad00b285c5a235fe8dc3
Alexandr Kirilov <alexandr.kirilov@post.com>
2023-11-29 23:17:39 +0300

    ARBXMPL-356 Publish Rust example: Learning DataTypes v1

---
+++ rust/Learning/DataTypes_v1/src/main.rs
@@ -0,0 +1,66 @@
+// --------------------------------------
+// Test application
+// --------------------------------------
+
+// System includes
+
+// Application
+#![allow(non_snake_case)]
+fn main() {
+
+	let oNumber_u8_MAX: u8 = std::u8::MAX;
+	let oNumber_u8_MIN: u8 = std::u8::MIN;
+	println!("Number u8 MIN {oNumber_u8_MIN} MAX {oNumber_u8_MAX}");
+
+	let oNumber_u16_MAX: u16 = std::u16::MAX;
+	let oNumber_u16_MIN: u16 = std::u16::MIN;
+	println!("Number u16 MIN {oNumber_u16_MIN} MAX {oNumber_u16_MAX}");
+
+	let oNumber_u32_MAX: u32 = std::u32::MAX;
+	let oNumber_u32_MIN: u32 = std::u32::MIN;
+	println!("Number u32 MIN {oNumber_u32_MIN} MAX {oNumber_u32_MAX}");
+
+	let oNumber_u64_MAX: u64 = std::u64::MAX;
+	let oNumber_u64_MIN: u64 = std::u64::MIN;
+	println!("Number u64 MIN {oNumber_u64_MIN} MAX {oNumber_u64_MAX}");
+
+	let oNumber_u128_MAX: u128 = std::u128::MAX;
+	let oNumber_u128_MIN: u128 = std::u128::MIN;
+	println!("Number u64 MIN {oNumber_u128_MIN} MAX {oNumber_u128_MAX}");
+
+	let oNumber_i8_MAX: i8 = std::i8::MAX;
+	let oNumber_i8_MIN: i8 = std::i8::MIN;
+	println!("Number i8 MIN {oNumber_i8_MIN} MAX {oNumber_i8_MAX}");
+
+	let oNumber_i16_MAX: i16 = std::i16::MAX;
+	let oNumber_i16_MIN: i16 = std::i16::MIN;
+	println!("Number i16 MIN {oNumber_i16_MIN} MAX {oNumber_i16_MAX}");
+
+	let oNumber_i32_MAX: i32 = std::i32::MAX;
+	let oNumber_i32_MIN: i32 = std::i32::MIN;
+	println!("Number i32 MIN {oNumber_i32_MIN} MAX {oNumber_i32_MAX}");
+
+	let oNumber_i64_MAX: i64 = std::i64::MAX;
+	let oNumber_i64_MIN: i64 = std::i64::MIN;
+	println!("Number i64 MIN {oNumber_i64_MIN} MAX {oNumber_i64_MAX}");
+
+	let oNumber_i128_MAX: i128 = std::i128::MAX;
+	let oNumber_i128_MIN: i128 = std::i128::MIN;
+	println!("Number i128 MIN {oNumber_i128_MIN} MAX {oNumber_i128_MAX}");
+
+	let oNumber_f32_MAX: f32 = std::f32::MAX;
+	let oNumber_f32_MIN: f32 = std::f32::MIN;
+	println!("Number f32 MIN {oNumber_f32_MIN} MAX {oNumber_f32_MAX}");
+
+	let oNumber_f64_MAX: f64 = std::f64::MAX;
+	let oNumber_f64_MIN: f64 = std::f64::MIN;
+	println!("Number f64 MIN {oNumber_f64_MIN} MAX {oNumber_f64_MAX}");
+
+	let oNumber_usize_MAX: usize = std::usize::MAX;
+	let oNumber_usize_MIN: usize = std::usize::MIN;
+	println!("Number usize MIN {oNumber_usize_MIN} MAX {oNumber_usize_MAX}");
+
+	let oNumber_isize_MAX: isize = std::isize::MAX;
+	let oNumber_isize_MIN: isize = std::isize::MIN;
+	println!("Number isize MIN {oNumber_isize_MIN} MAX {oNumber_isize_MAX}");
+}
