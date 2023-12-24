3135ad0ae9ff6ae8e4d3bd8f79556a59251a0f8c
Alexandr Kirilov <alexandr.kirilov@post.com>
2023-12-24 11:34:34 +0300

    ARBXMPL-370 Publish Rust example: Learning Enum v3

--- rust/Learning/Enum/Enum_v3/src/main.rs
+++ rust/Learning/Enum/Enum_v3/src/main.rs
@@ -9,6 +9,27 @@ enum A_ENUM {
 	STRUCT { a: bool },
 }
 
+impl A_ENUM {
+
+	fn mDiscriminant(&self) -> u8 {
+
+		unsafe { *<*const _>::from(self).cast::<u8>() }
+	}
+}
+
 fn main() {
-    println!("Hello, world!");
+
+	let oEnumUnit = A_ENUM::UNIT;
+	assert_eq!(0, oEnumUnit.mDiscriminant());
+	println!("Enum: {:?} Discriminant: {}", oEnumUnit, oEnumUnit.mDiscriminant());
+
+	let oEnumTuple = A_ENUM::TUPLE(true);
+	assert_eq!(1, oEnumTuple.mDiscriminant());
+	println!("Enum: {:?} Discriminant: {}", oEnumTuple, oEnumTuple.mDiscriminant());
+
+	let oEnumStruct = A_ENUM::STRUCT { a: false };
+	assert_eq!(2, oEnumStruct.mDiscriminant());
+	println!("Enum: {:?} Discriminant: {}", oEnumStruct, oEnumStruct.mDiscriminant());
+
+	println!("Enum_v3");
 }
