45ae1ec3c726747eb4227af47fa165132b24764b
Alexandr Kirilov <alexandr.kirilov@post.com>
2024-03-03 17:09:00 +0300

    ARBXMPL-387 Publish Perl example: HelloWorld v1

---
+++ perl/HelloWorld/HelloWorld_v1/hello_world_v1/lib/hello_world.pl
@@ -0,0 +1,8 @@
+# -----------------------
+# use command to run it without shebang
+# $ perl -I$PATH_TO_PROJECT/hello_world_v1/lib $PATH_TO_PROJECT/hello_world_v1/lib/hello_world.pl
+
+use strict;
+use warnings FATAL => 'all';
+
+print "Hello world!\n";
--- perl/HelloWorld/HelloWorld_v1/lib/hello_world.pl
+++
@@ -1,4 +0,0 @@
-use strict;
-use warnings FATAL => 'all';
-
-print "Hello world!\n";
