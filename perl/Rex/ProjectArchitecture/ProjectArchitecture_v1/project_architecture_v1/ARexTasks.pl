f57504f3c51cb3ce3fba1f64f38474a290cc624b
Alexandr Kirilov <alexandr.kirilov@post.com>
2024-03-30 11:49:02 +0300

    ARBXMPL-417 Publish Perl/Rex example: ProjectArchitecture_v1

--- perl/Rex/ProjectArchitecture/ProjectArchitecture_v1/project_architecture_v1/ARexTasks.pl
+++ perl/Rex/ProjectArchitecture/ProjectArchitecture_v1/project_architecture_v1/ARexTasks.pl
@@ -1,19 +1,26 @@
 #!/usr/bin/perl
 
 use strict;
-use warnings FATAL => 'all';
-
-use Rex -feature => [ '1.4' ];
+use warnings;
+use feature qw(say);
 
 BEGIN {
 
 	use Cwd qw();
 	push(@INC,Cwd::cwd());
 	push(@INC,Cwd::cwd()."/AStructure");
+	push(@INC,Cwd::cwd()."/AModule1");
 };
 
 use AStructure::Servers;
 use AStructure::Groups;
+
+# use AModule1::Package1 qw(:default);
+# use AModule1::Package1 qw(:all);
+use AModule1::Package1;
+
+say(AModule1::Package1::mSumm(10,20));
+say(AModule1::Package1::mTestString);
 
 Rex::Commands::desc("Get uptime server");
 Rex::Commands::task('uptime',
