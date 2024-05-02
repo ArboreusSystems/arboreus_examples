52c7652a9a00938d472af05d3f06cf87ac6a90a2
Alexandr Kirilov <alexandr.kirilov@post.com>
2024-05-02 20:12:55 +0300

    ARBXMPL-428 Publish Perl example: YAML v1

--- perl/YAML/YAML_v1/yaml_v1/main.pl
+++ perl/YAML/YAML_v1/yaml_v1/main.pl
@@ -4,7 +4,10 @@ use strict;
 use warnings FATAL => 'all';
 use Data::Dumper;
 use feature qw/say/;
+
 use YAML;
+use YAML::XS;
+use Cwd;
 
 say YAML::Dump( { ARRAY => ["str1","str2","str3"] });
 say YAML::Dump( { WRONG_LIST => "str1","str2","str3" });
@@ -16,5 +19,20 @@ say YAML::Dump( {
 		KEY1 => "VALUE2"
 	}
 });
+
+my $oFile = Cwd::cwd()."/dump.yaml";
+if (-f $oFile) {
+	CORE::unlink($oFile);
+}
+YAML::DumpFile($oFile,{
+	key1 => "value1",
+	key2 => 2,
+	key3 => [1,2,3,4],
+	key4 => {
+		key5 => 5,
+		key6 => "six"
+	}
+});
+say(Dump(YAML::XS::LoadFile($oFile)));
 
 1;
