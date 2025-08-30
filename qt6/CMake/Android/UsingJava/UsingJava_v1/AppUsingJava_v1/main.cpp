5253a36ead64e1f08e8e5b8b7467e8058ef022e8
Alexandr Kirilov <alexandr.kirilov@post.com>
2025-08-30 19:25:48 +0300

    ARBXMPL-488 Intermediate commit

---
+++ qt6/CMake/Android/UsingJava/UsingJava_v1/AppUsingJava_v1/main.cpp
@@ -0,0 +1,47 @@
+/* -------------------------------------------------------------------
+ *  @doc
+ *  @notice Template file tablemodel.h
+ *
+ *  @copyright Arboreus (http://arboreus.systems)
+ *  @author Alexandr Kirilov (http://alexandr.kirilov.me)
+ *  @created 30/08/2025 at 19:09:05
+ * */// --------------------------------------------------------------
+
+
+// Precompiled includes
+#include <main_pch.h>
+
+// System includes
+
+// Application includes
+
+// Constants
+#define CLIENT_QML_MAIN "qrc:/Main.qml"
+
+
+// Qt Quick Application
+int main(int inCounter, char *inArguments[]) {
+
+	QGuiApplication oApplicationObject(inCounter, inArguments);
+	QGuiApplication* oApplication = &oApplicationObject;
+	QQmlApplicationEngine oEngineObject;
+	QQmlApplicationEngine* oEngine = &oEngineObject;
+
+	qInstallMessageHandler(fLoggerMessageHandler);
+
+	_A_DEBUG << "test";
+
+	const QUrl oURL(QStringLiteral(CLIENT_QML_MAIN));
+	QObject::connect(
+		oEngine, &QQmlApplicationEngine::objectCreated,
+		oApplication, [oURL](QObject* obj, const QUrl& objUrl) {
+			if (!obj && oURL == objUrl) {
+				QCoreApplication::exit(-1);
+			}
+		}, Qt::QueuedConnection
+	);
+	oEngine->load(oURL);
+	int oExecutionResult = oApplication->exec();
+
+	return oExecutionResult;
+}
---
+++ qt6/CMake/Android/UsingJava/UsingJava_v1/AppUsingJava_v1/main_pch.h
@@ -0,0 +1,31 @@
+/* -------------------------------------------------------------------
+ *  @doc
+ *  @notice Template file apch.h
+ *
+ *  @copyright Arboreus (http://arboreus.systems)
+ *  @author Alexandr Kirilov (http://alexandr.kirilov.me)
+ *  @created 30/08/2025 at 19:09:05
+ * */// --------------------------------------------------------------
+
+#ifndef MAIN_PCH_H
+#define MAIN_PCH_H
+
+#if defined __cplusplus
+
+#pragma once
+
+// System includes
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+
+// Application includes
+#include <aloggerglobal.h>
+
+// Constants and defintions
+
+// Namespace
+
+
+#endif // defined __cplusplus
+
+#endif // MAIN_PCH_H
