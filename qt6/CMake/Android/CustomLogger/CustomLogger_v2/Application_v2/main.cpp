665b39300a970b1f5862f90014c36ab749d76605
Alexandr Kirilov <alexandr.kirilov@post.com>
2025-08-31 20:53:28 +0300

    ARBXMPL-490 Intermediate commit

---
+++ qt6/CMake/Android/CustomLogger/CustomLogger_v2/Application_v2/main.cpp
@@ -0,0 +1,35 @@
+/* -------------------------------------------------------------------
+ *  @doc
+ *  @notice Template file tablemodel.h
+ *
+ *  @copyright Arboreus (http://arboreus.systems)
+ *  @author Alexandr Kirilov (http://alexandr.kirilov.me)
+ *  @created 31/08/2025 at 20:39:20
+ * */// --------------------------------------------------------------
+
+
+// Precompiled includes
+#include <main_pch.h>
+
+// System includes
+
+// Application includes
+
+
+// Qt Quick Application
+int main(int inCounter, char *inArguments[]) {
+
+	QGuiApplication oApplication(inCounter, inArguments);
+	QQmlApplicationEngine oEngine;
+
+	const QUrl oURL = QUrl(QStringLiteral("qrc:/Main.qml"));
+	QObject::connect(
+				&oEngine, &QQmlApplicationEngine::objectCreationFailed,
+				&oApplication, []() {
+		QCoreApplication::exit(-1);
+	},
+	Qt::QueuedConnection);
+	oEngine.load(oURL);
+
+	return oApplication.exec();
+}
---
+++ qt6/CMake/Android/CustomLogger/CustomLogger_v2/Logger_v2/logger_v2.h
@@ -0,0 +1,32 @@
+// ----------------------------------------------------------
+/*!
+	\headerfile Logger_v2
+	\title
+	\brief Template file wizard/classes/cpp/file.h
+
+	\list
+	\li @notice Template file classes/file.h
+	\li @copyright Arboreus (http://arboreus.systems)
+	\li @author Alexandr Kirilov (http://alexandr.kirilov.me)
+	\li @created 31/08/2025 at 20:40:39
+	\endlist
+*/
+// ----------------------------------------------------------
+
+#ifndef LOGGER_V2_H
+#define LOGGER_V2_H
+
+// Precompiled includes
+#include <logger_v2_pch.h>
+
+// System includes
+
+class Logger_v2 {
+
+	public:
+
+		Logger_v2(void);
+		~Logger_v2(void);
+};
+
+#endif // LOGGER_V2_H
