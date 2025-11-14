78397c69f3db2b3094b58feec824195193468fad
Alexandr Kirilov <alexandr.kirilov@post.com>
2025-11-14 19:39:22 +0300

    ARBXMPL-505 Publish Qt example: Android UsingCPPInJava v2

---
+++ qt6/CMake/Android/UsingCPPInJava/UsingCPPInJava_v2/AppUsingCPPInJava_v2/Platforms/android/src/systems/arboreus/AJavaPackage/AJavaClassCallback.java
@@ -0,0 +1,7 @@
+package systems.arboreus.AJavaPackage;
+
+
+public class AJavaClassCallback {
+
+	native public static int mTestMethod(int inNumber);
+}
---
+++ qt6/CMake/Android/UsingCPPInJava/UsingCPPInJava_v2/AppUsingCPPInJava_v2/main.cpp
@@ -0,0 +1,81 @@
+/* -------------------------------------------------------------------
+ *  @doc
+ *  @notice Template file tablemodel.h
+ *
+ *  @copyright Arboreus (http://arboreus.systems)
+ *  @author Alexandr Kirilov (http://alexandr.kirilov.me)
+ *  @created 30/08/2025 at 19:09:05
+ * */// --------------------------------------------------------------
+
+
+// Precompiled includes
+#include <main_pch.h>
+
+// System includes
+
+// Application includes
+#include <aloggerglobal.h>
+#include <Backend/abackend.h>
+
+// Constants
+#define CLIENT_QML_MAIN "qrc:/Main.qml"
+
+
+// Java native callback funcion
+Q_DECLARE_JNI_CLASS(AJavaClassCallback,"systems/arboreus/AJavaPackage/AJavaClassCallback")
+
+static jint mTestMethod(JNIEnv* inEnvironment, jobject inObject, jint inNumber) {
+
+	Q_UNUSED(inEnvironment);
+	Q_UNUSED(inObject);
+
+	_A_DEBUG << "JAVA native callback:" << inNumber;
+
+	return inNumber * 3;
+}
+
+Q_DECLARE_JNI_NATIVE_METHOD(mTestMethod)
+
+
+// Java native callback funcion declaration
+Q_DECL_EXPORT jint JNICALL JNI_OnLoad(JavaVM* inJavaVM, void* inReserved) {
+
+	Q_UNUSED(inJavaVM);
+	Q_UNUSED(inReserved);
+
+	QJniEnvironment oJNIEnvironment;
+	oJNIEnvironment.registerNativeMethods<QtJniTypes::AJavaClassCallback>({
+		Q_JNI_NATIVE_METHOD(mTestMethod)
+	});
+
+	return JNI_VERSION_1_6;
+}
+
+
+// Qt Quick Application
+int main(int inCounter, char *inArguments[]) {
+
+	QGuiApplication oApplicationObject(inCounter, inArguments);
+	QGuiApplication* oApplication = &oApplicationObject;
+	QQmlApplicationEngine oEngineObject;
+	QQmlApplicationEngine* oEngine = &oEngineObject;
+
+	qInstallMessageHandler(fLoggerMessageHandler);
+
+	ABackend* oBackend = &ABackend::mInstance();
+	oBackend->mInit(oApplication,oEngine,oEngine->rootContext());
+
+	const QUrl oURL(QStringLiteral(CLIENT_QML_MAIN));
+	QObject::connect(
+		oEngine, &QQmlApplicationEngine::objectCreated,
+		oApplication, [oURL](QObject* obj, const QUrl& objUrl) {
+			if (!obj && oURL == objUrl) {
+				QCoreApplication::exit(-1);
+			}
+		}, Qt::QueuedConnection
+	);
+	oEngine->load(oURL);
+
+	int oExecutionResult = oApplication->exec();
+	return oExecutionResult;
+}
